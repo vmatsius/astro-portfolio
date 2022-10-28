0d635db4f223992e5c9a4209c30f3986bd5ce181
vmatsius <vmatsius@ucdavis.edu>
2022-10-28 20:06:29 +0000

    deploy

--- examples/portfolio/astro.config.mjs
+++ examples/portfolio/astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://vmatsius.github.io',
+  base: '/astro-portfolio/examples/portfolio/',
+})
